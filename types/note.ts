e3902101d00c28bc6c8e4c74f90b57db9be90a63
Vonels <klizka38@gmail.com>
2026-01-20 15:55:19 +0200

    add base

--- types/note.ts
+++ types/note.ts
@@ -1,23 +1,16 @@
-export type NoteTag = "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
-
 export interface Note {
   id: string;
   title: string;
   content: string;
+  createdAt: string;
+  updatedAt: string;
   tag: string;
-  createdAt?: string;
-  updatedAt?: string;
-}
-export interface NoteFormData {
-  title: string;
-  content: string;
-  tag: NoteTag;
 }
 
-export interface FetchNotesResponse {
-  notes: Note[];
-  totalPages: number;
-  totalNotes: number;
+export interface NoteFormValues {
+  title: string;
+  content: string;
+  tag: string;
 }
 
 export interface FetchNotesParams {
@@ -25,14 +18,4 @@ export interface FetchNotesParams {
   page?: number;
   perPage?: number;
   tag?: string;
-}
-
-export interface RegisterCredentials {
-  email: string;
-  password: string;
-}
-
-export interface LoginCredentials {
-  email: string;
-  password: string;
 }
