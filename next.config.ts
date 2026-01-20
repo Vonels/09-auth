8037c7195c37d2c866d5b0af71277da58956e216
Vonels <klizka38@gmail.com>
2026-01-20 19:16:30 +0200

    add base

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactCompiler: true,
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "ac.goit.global",
+        pathname: "/**",
       },
     ],
   },
